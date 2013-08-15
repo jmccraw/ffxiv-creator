22eed19c352070853f9269200de2d9c29f11ed39
Justin McCraw <jmccraw1@gmail.com>
2013-08-15 00:08:32 -0400

    Character races

--- js/creator.js
+++ js/creator.js
@@ -1,6 +1,14 @@
 ;(function($) {
 	"use strict";
 	
-	
+	$(".raceContainer").on("click.raceClick touchend.raceTouch", ".checkBox", function() {
+		var self = $(this).prev();
+		if (self.hasClass("racePortraitReveal")) {
+			self.removeClass("racePortraitReveal");
+		}
+		else {
+			self.addClass("racePortraitReveal");
+		}
+	});
 	
 })(jQuery);
